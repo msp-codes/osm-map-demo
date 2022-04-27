9c166a772f9d8f45a3677d8521d6ad6a2ec628ba
MSP <msp@MacBook-Pro.local>
2022-04-27 18:45:03 +0700

    Init request

---
+++ script.js
@@ -0,0 +1,64 @@
+class Map {
+
+    init() {
+        return L.map('map-container', {
+            center: [1.290270, 103.851959],
+            zoom: 12,
+            maxZoom: 25
+        });
+    }
+
+    createBaseLayer() {
+        return L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+    }
+
+    async createPinLayer() {
+
+        const sgData = await this.fetchData();
+
+        const cluster = L.markerClusterGroup()
+
+        for(let i=0; i < sgData.length; i++) {
+            const data = sgData[i];
+            let coords = [data.latitude, data.longitude];
+            cluster.addLayer(this.createMarker(coords, data))
+        }
+
+        return cluster;
+    }
+
+    createMarker(coords, data) {
+
+        return L.marker(coords, {
+            data: data,
+            icon: this.createCustomPin(data)
+        })
+    }
+
+    createCustomPin(data) {
+        const templ = `
+        <div class="custom-pin">
+            ${data.price}
+        </div>
+        `;
+        return L.divIcon({
+            html: templ
+        })
+    }
+
+    async fetchData() {
+        const res = await fetch('singapore-listing.json').then( res => res.json())
+        return res;
+    }
+}
+
+(async (map) => {
+    var mapObj = map.init();
+
+    // Add base layer tile to map
+    mapObj.addLayer(map.createBaseLayer());
+    mapObj.addLayer(await map.createPinLayer());
+
+})(new Map());
+
+
