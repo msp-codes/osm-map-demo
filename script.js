1bc585cab115a607d4c8ca9932b6eb1075218a1b
MSP <msp@MacBook-Pro.local>
2022-05-05 16:29:02 +0700

    remove map init

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 class Map {
-
-    init() {
-        return L.map('map-container', {
-            center: [1.290270, 103.851959],
-            zoom: 12,
-            maxZoom: 25
-        });
-    }
-
 }
 
 (async (map) => {
-    var mapObj = map.init();
 
 })(new Map());
 
