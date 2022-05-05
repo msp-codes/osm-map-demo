4441ac6d6c473aae9219fe61863516e7c6d73d99
MSP <msp@MacBook-Pro.local>
2022-05-05 16:17:35 +0700

    remove pins

--- script.js
+++ script.js
@@ -12,21 +12,6 @@ class Map {
         return L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
     }
 
-    async createPinLayer() {
-
-        const sgData = await this.fetchData();
-
-        const pins = sgData.map( data => {
-            const coords = [data.latitude, data.longitude];
-            return this.createMarker(coords);
-        })
-
-        return L.layerGroup(pins);
-    }
-
-    createMarker(coords) {
-        return L.marker(coords);
-    }
 
     async fetchData() {
         const res = await fetch('singapore-listing.json').then( res => res.json())
@@ -39,7 +24,6 @@ class Map {
 
     // Add base layer tile to map
     mapObj.addLayer(map.createBaseLayer());
-    mapObj.addLayer(await map.createPinLayer());
 
 })(new Map());
 
